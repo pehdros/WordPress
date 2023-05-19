0e8e1fa0e1596fb7a4ba25f31d584c95de25aaaa
pehdros <pehdros@gmail.com>
2023-05-18 23:49:26 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/general-template.php
+++ wordpress/wp-content/themes/paperless/general-template.php
@@ -6,12 +6,11 @@ Template Name: Template Geral
 
 <?php get_header(); ?>
 
-
-
+<!-- O administrador poderá incluir promoções -->
 <section>
     <div class="container">
         <div class="row">
-        <?php
+            <?php
             if (is_active_sidebar('promocao')) {
                 dynamic_sidebar('promocao');
             }
@@ -20,20 +19,28 @@ Template Name: Template Geral
     </div>
 </section>
 
-<div class="">
-    <div class="row">
-        <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-        ?>
-
-        <?php get_template_part('template-parts/content', 'quote'); ?>           
-
-        <?php
-            endwhile;
-        endif;
-        ?>
+<!-- O administrador poderá incluir conteúdo do post e informações em uma sidebar lateral -->
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post();
+                        the_content();
+                    endwhile;
+                endif;
+                ?>
+            </div>
+            <div class="col-3 bg-success">
+                <?php
+                if (is_active_sidebar('sidebar-direita')) {
+                    dynamic_sidebar('sidebar-direita');
+                }
+                ?>
+            </div>
+        </div>
     </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
