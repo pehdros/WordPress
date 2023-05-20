ab2a7dbc64a8563a0590ca4148a37361ca625f30
pehdros <pehdros@gmail.com>
2023-05-20 01:18:28 -0300

    estilos sidebar categorias

--- wordpress/wp-content/themes/paperless/functions.php
+++ wordpress/wp-content/themes/paperless/functions.php
@@ -149,7 +149,7 @@ function registrando_sidebars()
             'name' => 'Categorias',
             'id' => 'categorias',
             'description' => 'Área de categorias para Blog',
-            'before_widget' => '<div class="">',
+            'before_widget' => '<div class="container my-5">',
             'after_widget' => '</div>',
             'before_title' => '<h2 class="">',
             'after_title' => '</h2>'
@@ -205,13 +205,3 @@ function registrando_sidebars()
     );
 }
 
-/**
- * Filter the except length to 20 words.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
- */
-function wpdocs_custom_excerpt_length( $length ) {
-	return 10;
-}
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length',  );
