b4d16ac32e2a20a735bbb4c8f2d4efd8783aba1f
pehdros <pehdros@gmail.com>
2023-05-20 01:19:49 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/template-parts/content-secondary.php
+++ wordpress/wp-content/themes/paperless/template-parts/content-secondary.php
@@ -1,14 +1,17 @@
-<article <?php post_class(['class' => 'card mt-1 ']); ?>>
+<!-- Referente a Blog em page-home.php -->
+<article <?php post_class(['class' => 'card my-3']); ?>>
     <div class="row card-body g-0">
-        <div class="col-md-4">
-            <a href="<?php the_permalink();?>" class="text-decoration-none"><?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?></a>
+        <div class="col-md-5">
+            <a href="<?php the_permalink(); ?>" class="text-decoration-none"><?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?></a>
         </div>
-        <div class="col-md-8 text-primary">
-            <div class="px-3 pt-2">
+        <div class="col-md-7 text-primary px-4">
+            <div class="pb-3">
                 <h3 class="card-title "><?php the_title(); ?></h3>
-                <span class="opacity-50"><?php echo get_the_date(); ?></span>
-                <p class="card-text"><?php the_excerpt(); ?></p>
-                <a href="<?php the_permalink();?>" class="btn btn-success text-decoration-none">Leia mais</a>
+                <span class="opacity-50"><?php echo get_the_date();?></span>
+            </div>
+            <div class="">
+                <p class="card-text"><?php echo get_the_excerpt(); ?></p>
+                <a href="<?php the_permalink(); ?>" class="btn btn-success text-decoration-none">Leia mais</a>
             </div>
         </div>
     </div>
