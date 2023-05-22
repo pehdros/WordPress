7958e6c45ab58af3cad38715eae251a2a01c5d94
pehdros <pehdros@gmail.com>
2023-05-22 15:03:56 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/header.php
+++ wordpress/wp-content/themes/paperless/header.php
@@ -12,30 +12,33 @@
 
     <header>
         <!-- Sidebar topo - social icons & search -->
-        <section class="bg-primary">
+        <section class="bg-info">
             <?php get_sidebar('topo'); ?>
         </section>
-        <nav class="navbar navbar-expand-md bg-success">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md bg-primary">
             <div class="container">
-                <a class="" href="#">
+                <div class="">
                     <?php the_custom_logo(); ?>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                </div>
+                <div class="">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="main-menu">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'main-menu',
-                        'container' => false,
-                        'menu_class' => '',
-                        'fallback_cb' => '__return_false',
-                        'items_wrap' => '<ul id="%1$s" class="navbar-nav menu me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-                        'depth' => 2,
-                        'walker' => new bootstrap_5_wp_nav_menu_walker()
-                    ));
-                    ?>
+                    <div class="collapse navbar-collapse" id="main-menu">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'main-menu',
+                            'container' => false,
+                            'menu_class' => '',
+                            'fallback_cb' => '__return_false',
+                            'items_wrap' => '<ul id="%1$s" class="navbar-nav menu me-auto mb-md-0 fs-5 %2$s">%3$s</ul>',
+                            'depth' => 2,
+                            'walker' => new bootstrap_5_wp_nav_menu_walker()
+                        ));
+                        ?>
+                    </div>
                 </div>
             </div>
         </nav>
