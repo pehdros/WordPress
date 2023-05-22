40c0253838ece8c77089c9171104fa16c68fee71
pehdros <pehdros@gmail.com>
2023-05-22 15:07:10 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/page-home.php
+++ wordpress/wp-content/themes/paperless/page-home.php
@@ -22,56 +22,7 @@
     </div>
 </section>
 
-<!-- Últimos posts seguindo as categorias informadas -->
-<section class="blog my-5">
-    <div class="container">
-        <div class="text-center">
-            <h2 class="text-primary py-2">Blog</h2>
-            <p>Conheça o nosso blog e fique por dentro das novidades na área da estética e acupuntura</p>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <?php
-                // Exibe uma categoria de post nesta página
-                $featured = new WP_Query('post_type=post&posts_per_page=1&cat=28');
-                if ($featured->have_posts()) :
-                    while ($featured->have_posts()) : $featured->the_post();
-                ?>
 
-                        <div class="col-12">
-                            <?php get_template_part('template-parts/content', 'featured') ?>
-                        </div>
 
-                <?php
-                    endwhile;
-                    wp_reset_postdata();
-                endif
-                ?>
-            </div>
-            <div class="col-md-8">
-                <?php
-                // Exibe duas categorias de post nesta página
-                $args = [
-                    'post_type' => 'post',
-                    'posts_per_page' => 2,
-                    'category__not_in' => [28],
-                    'category__in' => [25, 29]
-                ];
-                $secondary = new WP_Query($args);
-                if ($secondary->have_posts()) :
-                    while ($secondary->have_posts()) : $secondary->the_post();
-                ?>
-                        <div class="col-12">
-                            <?php get_template_part('template-parts/content', 'secondary') ?>
-                        </div>
-                <?php
-                    endwhile;
-                    wp_reset_postdata();
-                endif
-                ?>
-            </div>
-        </div>
-    </div>
-</section>
 
 <?php get_footer(); ?>
