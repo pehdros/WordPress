a3f169e2a6cdddb15192d9a78951859cb033edd9
pehdros <pehdros@gmail.com>
2023-05-22 15:03:06 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/footer.php
+++ wordpress/wp-content/themes/paperless/footer.php
@@ -1,27 +1,23 @@
-<footer class="">
+<footer class="bg-success">
+    <nav class="navbar navbar-expand-md ">
+        <div class="container">
+            <?php the_custom_logo(); ?>
 
-    <section class="bg-success">
-        <nav class="navbar navbar-expand-md ">
-            <div class="container">
-                <span class="text-primary"> <?php the_custom_logo(); ?> </span>
-
-                <div class="" id="main-menu">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'menu-footer',
-                        'container' => false,
-                        'menu_class' => '',
-                        'fallback_cb' => '__return_false',
-                        'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto menu-footer mb-2 mb-md-0 %2$s">%3$s</ul>',
-                        'depth' => 2,
-                        'walker' => new bootstrap_5_wp_nav_menu_walker()
-                    ));
-                    ?>
-                </div>
+            <div class="" id="main-menu">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'menu-footer',
+                    'container' => false,
+                    'menu_class' => '',
+                    'fallback_cb' => '__return_false',
+                    'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto fs-5 mb-2 mb-md-0 %2$s">%3$s</ul>',
+                    'depth' => 2,
+                    'walker' => new bootstrap_5_wp_nav_menu_walker()
+                ));
+                ?>
             </div>
-        </nav>
-    </section>
-
+        </div>
+    </nav>
 </footer>
 <?php wp_footer(); ?>
 </body>
