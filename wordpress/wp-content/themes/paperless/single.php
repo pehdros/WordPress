c65b4959d2457989feeee66bfe65c2796ac77ce3
pehdros <pehdros@gmail.com>
2023-05-24 17:03:43 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/single.php
+++ wordpress/wp-content/themes/paperless/single.php
@@ -1,30 +1,29 @@
 <?php get_header(); ?>
 
-<div class="container">
-<?php 
-while(have_posts()) :the_post();
+<?php
+while (have_posts()) : the_post();
 
     get_template_part('template-parts/content', 'single');
 
-    ?>
-
-    <div class="row">
-        <div class="text-start col-6">
-            <?php next_post_link('&laquo; %link');?>
-        </div>
-        <div class="text-end col-6">
-            <?php previous_post_link('%link &raquo;');?>
+?>
+    <div class="container">
+        <div class="row">
+            <div class="text-start col-6">
+                <?php next_post_link('&laquo; %link'); ?>
+            </div>
+            <div class="text-end col-6">
+                <?php previous_post_link('%link &raquo;'); ?>
+            </div>
         </div>
     </div>
+<?php
 
-    <?php
-
-    if(comments_open() || get_comments_number() ) :
+    if (comments_open() || get_comments_number()) :
         comments_template();
     endif;
 
 endwhile;
 ?>
-</div>
+
 
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/paperless/template-parts/content-single.php
+++ wordpress/wp-content/themes/paperless/template-parts/content-single.php
@@ -1,15 +1,14 @@
-<article id="post<?php the_ID(); ?><?php post_class(); ?>">
-    <header class="">
-        <h1><?php the_title(); ?></h1>
-        <div class="">
-            <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-            <p>Categorias: <?php the_category(['class' => 'text-decoration-none']); ?></p>
-            <p><?php the_tags(); ?></p>
+<section class="container my-5">
+    <div class="row justify-content-center mb-2">
+        <div class="col-12 col-lg-8">
+            <article class="" id="post<?php the_ID(); ?><?php post_class(); ?>">
+                <div class="categorias"><?php the_category(); ?></div>
+                <div class="text-primary py-3">
+                    <h1 class=""><?php the_title(); ?></h1>
+                    <p>Criado <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
+                </div>
+            </article>
+            <?php the_content();?>
         </div>
-    </header>
-
-    <div class="">
-        <?php the_content(); ?>
     </div>
-</article>
-
+</section>
