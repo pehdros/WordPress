7518b2b417d52c87bc5131d3dd1c11bb6ca2dc26
pehdros <pehdros@gmail.com>
2023-05-24 15:58:44 -0300

    finalizado

--- wordpress/wp-content/themes/paperless/archive.php
+++ wordpress/wp-content/themes/paperless/archive.php
@@ -1,22 +1,20 @@
 <?php get_header(); ?>
 
+<!-- Página para categorias -->
 
-<!-- BLOG -->
-<?php get_sidebar('categorias')?>
+<section class="py-5">
+    <?php get_sidebar('categorias') ?>
+</section>
+
 <div class="container">
     <div class="row">
         <?php
-
-        the_archive_title('<h1 class="text-center">','</h1>');
+        the_archive_title('<h2 class="text-center text-primary pb-4">', '</h2>');
         the_archive_description();
 
         if (have_posts()) :
             while (have_posts()) : the_post();
-        ?>
-
-                <?php get_template_part('template-parts/content', 'archive');?>
-
-        <?php
+                get_template_part('template-parts/content');
             endwhile;
         endif;
 
@@ -29,8 +27,5 @@
         ?>
     </div>
 </div>
-
-
-<!-- BLOG -->
 
 <?php get_footer(); ?>
