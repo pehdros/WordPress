714210e1331d460aeb255b8eae08131a84af8f34
pehdros <pehdros@gmail.com>
2023-05-25 22:39:30 -0300

    adicionando paginação ao content-single.php

--- wordpress/wp-content/themes/paperless/single.php
+++ wordpress/wp-content/themes/paperless/single.php
@@ -1,29 +1,21 @@
 <?php get_header(); ?>
 
+<!-- Conteúdo do post -->
 <?php
 while (have_posts()) : the_post();
 
     get_template_part('template-parts/content', 'single');
 
 ?>
-    <div class="container">
-        <div class="row">
-            <div class="text-start col-6">
-                <?php next_post_link('&laquo; %link'); ?>
-            </div>
-            <div class="text-end col-6">
-                <?php previous_post_link('%link &raquo;'); ?>
-            </div>
-        </div>
-    </div>
-<?php
 
-    if (comments_open() || get_comments_number()) :
-        comments_template();
-    endif;
+<!-- Comentários -->
+        <?php
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
 
-endwhile;
-?>
+            endwhile;
+    ?>
+    
 
-
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
