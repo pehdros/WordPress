cee7212ff0ae09c17201314ed590852852849339
pehdros <pehdros@gmail.com>
2023-05-26 22:59:59 -0300

    delete comments.php

--- wordpress/wp-content/themes/paperless/single.php
+++ wordpress/wp-content/themes/paperless/single.php
@@ -6,16 +6,8 @@ while (have_posts()) : the_post();
 
     get_template_part('template-parts/content', 'single');
 
+endwhile;
 ?>
 
-<!-- Comentários -->
-        <?php
-            if (comments_open() || get_comments_number()) :
-                comments_template();
-            endif;
 
-            endwhile;
-    ?>
-    
-
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
